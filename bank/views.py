8556b9d9906c1803541536940a10d3b005e60593
nevertheless0404 <nevertheless.hi@gmail.com>
2022-10-25 21:14:40 +0900

    bank/detail

--- bank/views.py
+++ bank/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+import imp
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import BankForm, CommentForm
+from .models import Bank, Comment
 
 # Create your views here.
 def index(request):
     return render(request, "bank/index.html")
+
+
+def detail(request):
+    return render(request, "bank/detail.html")
+
+
+def search(request):
+    return render(request, "bank/search.html")
+
+
+def words(request):
+    return render(request, "bank/words.html")
